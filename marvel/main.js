b99178e22195236d1681bfc9cb65d87711990161
melanykunzi <melanykunzi@gmail.com>
2023-05-10 16:16:23 -0300

    color

--- marvel/main.js
+++ marvel/main.js
@@ -4,7 +4,33 @@ var peliculasConFiltro = JSON.parse(localStorage.getItem("peliculasConFiltro")) 
 // Función para agregar una imagen a la lista de vistas
 function agregarImagen(img) {
   // Mostrar mensaje de confirmación
-  if (confirm("¿Agregar a Vistas?")) {
+  var alt = img.alt;
+  if (peliculasConFiltro.includes(alt)){
+    if (confirm("¿Borrar de vistas?")){
+      var index = peliculasConFiltro.indexOf(alt);
+
+      // Elimina el elemento del array usando splice()
+      peliculasConFiltro.splice(index, 1);
+  
+      // Guarda la lista actualizada en el almacenamiento local
+      localStorage.setItem("peliculasConFiltro", JSON.stringify(peliculasConFiltro));
+  
+      // Recorre todas las imágenes y cambia el filtro si el alt de la imagen está en la lista
+      var imagenes = document.getElementsByTagName("img");
+      for (var i = 0; i < imagenes.length; i++) {
+        var imagen = imagenes[i];
+        var alt = imagen.getAttribute("alt");
+        if (peliculasConFiltro.includes(alt)) {
+          imagen.style.filter = "grayscale(0%)";
+          imagen.style.border = "solid 1px red";
+        } else {
+          imagen.style.filter = "grayscale(100%)";
+          imagen.style.border = "none";
+        }
+      }
+    }
+  } else {
+      if (confirm("¿Agregar a Vistas?")) {
     // Obtener el alt de la imagen
     var alt = img.alt;
     
@@ -25,6 +51,8 @@ function agregarImagen(img) {
       }
     }
   }
+  }
+
 }
 // Recorre todas las imágenes y cambia el filtro si el alt de la imagen está en la lista
 var imagenes = document.getElementsByTagName("img");
